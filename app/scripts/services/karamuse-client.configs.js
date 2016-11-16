c0c891e9bdb0efc043ec20f3ed18a52a518561b7
Álvaro Troncoso <alvaro.mc2@gmail.com>
2016-11-16 11:16:24 -0300

    avance nuevo branding

--- app/scripts/services/karamuse-client.configs.js
+++ app/scripts/services/karamuse-client.configs.js
@@ -3,7 +3,23 @@
 angular
 	.module('karamuseClientApp')
 
-.config(function($authProvider, API_URL_BASE) {
+.config(function($authProvider, $mdThemingProvider, API_URL_BASE) {
 	$authProvider.loginUrl = API_URL_BASE + "/login";
 	$authProvider.tokenPrefix = '';
+
+	$mdThemingProvider
+		.theme('karamuseTheme')
+		.primaryPalette('blue', {
+			default: '500',
+			'hue-1': 'A100',
+		})
+		.accentPalette('orange', {
+			default: '600'
+		})
+		.warnPalette('red', {
+			default: '900'
+		})
+		.backgroundPalette('grey');
+
+	$mdThemingProvider.setDefaultTheme('karamuseTheme');
 });
